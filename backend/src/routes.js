2b4328e69260621493a2698e9acd51c0f9878a94
Marceloni Lima <markill123@gmail.com>
2019-09-30 23:36:45 -0300

    Criando o app do tindev

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
+routes.get('/devs', DevController.index);
+
 routes.get('/', (req, res) => {
   req.query.name;
-  return res.json({ message: `Olá ${req.query.name}` });
+  return res.json({ message: `Ol? ${req.query.name}` });
 });
 
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 mongoose.connect(
-  'mongodb+srv://omnistack:omnistack@cluster0-vnaya.mongodb.net/test?retryWrites=true&w=majority',
+  'mongodb+srv://omnistack:omnistack@cluster0-vnaya.mongodb.net/omnistack8?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
   },
 );
 
 const server = express();
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 

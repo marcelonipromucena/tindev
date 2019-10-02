34e1e1e0dec16015cf776682dd6ad592bef0f0e5
Marceloni Lima <markill123@gmail.com>
2019-10-01 21:12:32 -0300

    Indo para mobile

---
+++ tindev/src/routes.js
@@ -0,0 +1,9 @@
+import {
+  createAppContainer,
+  createSwitchNavigator,
+} from 'react-native-gesture-handler';
+
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+export default createAppContainer(createSwitchNavigator({}));

6c964f8cef7aec1920af8cb2f936b323c1e24b6d
Marceloni Lima <markill123@gmail.com>
2019-10-09 23:39:54 -0300

    App fully functional done

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # tindev
+
 #
-Projeto referente à semana omnistack
+
+Omnistack 8.0
+
+What this project is capable of:
+
+- Setup a fully-functional backend server with nodejs.
+- Serve endpoints which multiple frontends can consume (web & mobile).
+- Manipulate state with react.
+- Consume API's using React & React Native.
+- Business logic for relationship apps.
+
+![Alt text](screen-mobile-01.png?raw=true 'img')
+![Alt text](screen-mobile-02.png?raw=true 'img')
+![Alt text](screen-web-01.png?raw=true 'img')
+![Alt text](screen-web-02.png?raw=true 'img')
--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import {
   KeyboardAvoidingView,
   StyleSheet,
@@ -11,12 +13,29 @@ import {
 import api from '../services/api';
 
 import logo from '../assets/logo.png';
-export default function Login({navigation}) {
-  const [user, setUser] = useState('');
 
-  function handleLogin() {
-    console.log(user);
-    navigation.navigate('Main');
+export default function Login({navigation}) {
+  const [user, setUser] = useState('marcelonipromucena');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', {user});
+      }
+    });
+  }, []);
+
+  async function handleLogin() {
+    try {
+      const response = await api.post('/devs', {username: user});
+      const {_id} = response.data;
+
+      await AsyncStorage.setItem('user', _id);
+
+      navigation.navigate('Main', {user: _id});
+    } catch (error) {
+      console.error('Houve um erro', error);
+    }
   }
   return (
     <KeyboardAvoidingView
